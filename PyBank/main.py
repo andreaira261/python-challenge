7a9e5a16b2e340eecad438d0caa9c528edc10f6f
andreaiaguilar <andreaaguilar@Andreas-MBP.attlocal.net>
2023-07-17 15:05:57 -0700

    adding comments to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,39 +5,63 @@ import os
 current_directory = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_directory, 'Resources', 'budget_data.csv')
 
+# Lists to store data for total number of months and net total amount of "Profit/Losses"
 months_list = []
 total_list = []
 
+# with open as csvfile: 
 with open (file_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    # Store header row
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
 
     for row in csvreader:
+        # Store months in a list
         months_list.append(row[0])
 
+        # Store "Profit/Losses" in alist
         total_list.append(int(row[1]))
 
+# Caluculate total number of months
 months = len(months_list)
+
+# Calculate net total amount of "Profit/Losses" over the entire period 
 total = sum(total_list)
 
+# Set initial values for calculating greatest increase and decrease in profits 
 greatest_inc = 0
 greatest_dec = 0
+
+# List for storing "Profit/Losses" over the entire period and average of those changes
 avg_change_list = []
 
+# Loops through each month in dataset 
 for i in range(len(total_list)-1):
+    # Calculate changes in "Profit/Losses" on month by month basis
     diff = total_list[i+1] - total_list[i]
+
+    # Store changes in "Profit/Losses" on month by month basis in list 
     avg_change_list.append(diff)
+    
+    # Finds and stores greatest increase in profits 
     if diff > greatest_inc:
+        # Greatest increase amount
         greatest_inc = diff
+        # Index for month of greatest increase
         greatest_inc_row = i+1
+    
+    # Finds and stores greatest decrease in profits 
     elif diff < greatest_dec:
+        # Greatest decrease amount
         greatest_dec = diff
+        # Index for month of greatest decrease
         greatest_dec_row = i+1
 
+# Calculate average of changes in "Profit/Losses" over the entire peiod 
 avg_change = round(sum(avg_change_list) / len(avg_change_list),2)
 
+# Print results to terminal
 print("Financial Analysis")
 print ("-------------------------")
 
@@ -51,8 +75,10 @@ print (f"Greatest Increase in Profits: {months_list[greatest_inc_row]} (${greate
 
 print (f"Greatest Decrease in Profits: {months_list[greatest_dec_row]} (${greatest_dec})")
 
+# Variable and path to output file for printing results to text file     
 output_file = "/Users/andreaaguilar/python-challenge/PyBank/Analysis/results.txt"
 
+# Print results in text file
 with open(output_file, "w") as resultfile:
     resultfile.write("Financial Analysis\n")
     resultfile.write("-------------------------\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,35 +5,53 @@ import os
 current_directory = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_directory, 'Resources', 'election_data.csv')
 
+# List to store data for total number of votes cast
 candidates = []
 
+# with open as csvfile: 
 with open (file_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
+    # Store header
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
 
     for row in csvreader: 
-
+        # Store candidates row in a list
         candidates.append(row[2])
 
+# Calculate total number of votes cast
 total_votes = len(candidates)
 
+# List to store candidates who received votes 
 candidates_list = []
+
 for i in candidates:
+    # Stores unique candidates in list 
     if i not in candidates_list:
         candidates_list.append(i)
 
+# Lists to store data for total number of votes each candidate won and percentage of votes each candidate won 
 candidates_votes = []
 vote_percent = []
+
+# Loops through range of number of unique candidates
 for i in range(len(candidates_list)):
+    # Counts number of times each candidate received according to candidates_list
     votes = candidates.count(candidates_list[i])
+    
+    # Stores total number of votes for each candidate
     candidates_votes.append(votes)
+
+    # Calculates percentage of votes each candidate won
     percent = round((candidates_votes[i] / total_votes)*100,3)
+    
+    # Stores percentage of votes for each candidate
     vote_percent.append(percent)
- 
+
+# Index for candidate who received the most number of votes 
 winner_index = (candidates_votes.index(max(candidates_votes)))
 
+# Print results to terminal
 print ("Election Results")
 print ("-------------------------")
 
@@ -47,9 +65,11 @@ print ("-------------------------")
 print (f"Winner: {candidates_list[winner_index]}")
 
 print ("-------------------------")
-    
+
+# Variable and path to output file for printing results to text file     
 output_file = '/Users/andreaaguilar/python-challenge/PyPoll/Analysis/results.txt'
 
+# Pring results in text file
 with open(output_file, "w") as resultfile:
     resultfile.write("Election Results\n")
     resultfile.write("-------------------------\n")
